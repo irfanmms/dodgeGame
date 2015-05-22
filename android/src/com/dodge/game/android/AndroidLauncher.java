2b82a83231240b0e78f8dc771014476351527a21
irfanmms <irfanmms@hotmail.com>
2015-05-22 19:30:13 -0400

    implemented immersive mode hiding away navbar.

--- android/src/com/dodge/game/android/AndroidLauncher.java
+++ android/src/com/dodge/game/android/AndroidLauncher.java
@@ -2,6 +2,7 @@ package com.dodge.game.android;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.backends.android.AndroidApplication;
@@ -15,9 +16,22 @@ public class AndroidLauncher extends AndroidApplication{
 
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
+		hideSystemUi();
 		super.onCreate(savedInstanceState);
 		actionResolverAndroid = new ActionResolverAndroid(this);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.hideStatusBar=true;
+		config.useImmersiveMode=true;
 		initialize(new GameClass(actionResolverAndroid), config);
+	}
+
+	private void hideSystemUi() {
+		getWindow().getDecorView().setSystemUiVisibility(
+				View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+						| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+						| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+						| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+						| View.SYSTEM_UI_FLAG_FULLSCREEN
+						| View.SYSTEM_UI_FLAG_IMMERSIVE);
 	}
 }
